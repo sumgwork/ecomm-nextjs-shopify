39d129bfef10ae68ad789774079657f8b33046de
Sumit Govil <sumg.work@gmail.com>
2021-08-15 20:56:45 +1000

    Tailwind and PostCSS config

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
+import s from "./Layout.module.css";
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="layout">
-      <hr />
-      {children}
+    <div className={s.root}>
+      <main className="fit">{children}</main>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
+import "@assets/main.css";
 
-const Noop: FC = ({ children }) => <>{children}</>;
+const NoLayout: FC = ({ children }) => <>{children}</>;
 
 const MyApp = ({
   Component,
   pageProps,
 }: AppProps & { Component: { Layout: FC } }) => {
-  const Layout = Component.Layout ?? Noop;
+  const Layout = Component.Layout ?? NoLayout;
   return (
     <>
       <Layout>
